8047e42036b37ffe66adabe925b95cdebbaff908
Dennise Catolos <dentolos19@gmail.com>
2024-08-19 17:33:13 +0800

    Updated scripts

---
+++ scripts/build.ps1
@@ -0,0 +1,12 @@
+Set-Location "$PSScriptRoot/.."
+
+& $PSScriptRoot/setup.ps1
+Clear-Host
+
+Write-Host "Building the project..."
+Write-Host
+
+& pyinstaller src/app/main.py --onefile --icon src/icon.ico
+Remove-Item "main.spec"
+
+Copy-Item "public" "dist" -Recurse -Force
